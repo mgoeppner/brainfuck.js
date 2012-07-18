8a43403923ebebee96b1ca18292f363f251b683d
Mike Goeppner <mgoeppner@gmail.com>
2012-07-17 22:13:45 -0400

    Simple is nice.

--- brainfuck.js
+++ brainfuck.js
@@ -1,89 +1,44 @@
-﻿var brainfuck =
+﻿var memory = new Int8Array(new ArrayBuffer(30000));
+var pointer = 0;
+var loopPointer = 0;
+var program = "";
+var inLoop = 0;
+
+function exec(program)
+{
+    for (char = 0; char < program.length; char++)
     {
-        commands:
+        switch (program[char])
         {
-            '>': function (pointer)
-            {
-                return parseInt(pointer) + 1;
-            },
-            '<': function (pointer)
-            {
-                return parseInt(pointer) - 1;
-            },
-            '+': function (pointer, memory)
-            {
-                memory[pointer] = parseInt(memory[pointer]) + 1;
-                return pointer;
-            },
-            '-': function (pointer, memory)
-            {
-                memory[pointer] = parseInt(memory[pointer]) - 1;
-                return pointer;
-            },
-            '.': function (pointer, memory)
-            {
-                //console.log(String.fromCharCode(parseInt(memory[pointer] << 3)));
-                console.log((memory[pointer]));
-                return pointer;
-            },
-            ',': function (pointer, memory)
-            {
+            case ">":
+                pointer++;
+                break;
+            case "<":
+                pointer--;
+                break;
+            case "+":
+                memory[pointer]++;
+                break;
+            case "-":
+                memory[pointer]--;
+                break;
+            case ".":
+                console.log(String.fromCharCode(memory[pointer]));
+                break;
+            case ",":
                 var input = prompt("Enter a character:").charCodeAt(0);
                 memory[pointer] = input;
-                return pointer;
-            },
-            ']': function (pointer, memory, inst, program)
-            {
-                var newInst = 0;
-                var _pointer = pointer;
-
-                if (memory[_pointer] == 0)
-                {
-                    // Jump to after ']'
-                    newInst = program.indexOf(']', inst) + 1;
-                    _pointer = brainfuck.commands[program[newInst]](_pointer, memory, inst, program);
-                }
-                return _pointer;
-            },
-            '[': function (pointer, memory, inst, program)
-            {
-                var newInst = 0;
-                var _pointer = pointer;
-
-                if (memory[_pointer] != 0)
-                {
-                    // Run Loop Code
-                    newInst = program.lastIndexOf('[', inst) + 1;
-                    loop = program.slice(newInst, inst);
-                    for (char in loop)
-                    {
-                        if (typeof brainfuck.commands[loop[char]] === 'function')
-                            _pointer = brainfuck.commands[loop[char]](_pointer, memory, inst, program);
-                    }
-                }
-                return _pointer;
-            }
-        },
-        init: function ()
-        {
-            this.pointer = 0;
-            this.memory = new Int8Array(new ArrayBuffer(30000));
-        },
-        memory: [],
-        pointer: 0,
-        exec: function (program)
-        {
-            this.init();
-            var memory = this.memory;
-            var pointer = this.pointer;
-            var loop = "";
-            var inLoop = false;
-            var loopStack = [];
-
-            for (char in program)
-            {
-                if (typeof this.commands[program[char]] === 'function')
-                    pointer = this.commands[program[char]](pointer, memory, char, program);
-            }
+                break;
+            case "[":
+                if (memory[pointer] == 0)
+                    char = program.indexOf(']', char);
+                break;
+            case "]":
+                if (memory[pointer] != 0)
+                    char = program.lastIndexOf('[', char);
+                break;
         }
     }
+}
+
+exec(program);
