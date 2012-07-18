694fd2d4267aacf70b0829d92dfd6a9821540c88
Mike Goeppner <mgoeppner@gmail.com>
2012-07-17 20:28:28 -0400

    Memory update

--- brainfuck.js
+++ brainfuck.js
@@ -22,8 +22,8 @@
             },
             '.': function (pointer, memory)
             {
-                console.log(String.fromCharCode(parseInt(memory[pointer] << 3)));
-                //console.log((memory[pointer]));
+                //console.log(String.fromCharCode(parseInt(memory[pointer] << 3)));
+                console.log((memory[pointer]));
                 return pointer;
             },
             ',': function (pointer, memory)
@@ -35,18 +35,22 @@
             ']': function (pointer, memory, inst, program)
             {
                 var newInst = 0;
-                if (memory[pointer] == 0)
+                var _pointer = pointer;
+
+                if (memory[_pointer] == 0)
                 {
                     // Jump to after ']'
                     newInst = program.indexOf(']', inst) + 1;
-                    pointer = brainfuck.commands[program[newInst]](pointer, memory, inst, program);
+                    _pointer = brainfuck.commands[program[newInst]](_pointer, memory, inst, program);
                 }
-                return pointer;
+                return _pointer;
             },
             '[': function (pointer, memory, inst, program)
             {
                 var newInst = 0;
-                if (memory[pointer] != 0)
+                var _pointer = pointer;
+
+                if (memory[_pointer] != 0)
                 {
                     // Run Loop Code
                     newInst = program.lastIndexOf('[', inst) + 1;
@@ -54,20 +58,24 @@
                     for (char in loop)
                     {
                         if (typeof brainfuck.commands[loop[char]] === 'function')
-                           pointer = brainfuck.commands[loop[char]](pointer, memory, inst, program);
+                            _pointer = brainfuck.commands[loop[char]](_pointer, memory, inst, program);
                     }
                 }
-                return pointer;
+                return _pointer;
             }
         },
         init: function ()
         {
-            return new Int8Array(new ArrayBuffer(30000));
+            this.pointer = 0;
+            this.memory = new Int8Array(new ArrayBuffer(30000));
         },
+        memory: [],
+        pointer: 0,
         exec: function (program)
         {
-            var memory = this.init();
-            var pointer = 0;
+            this.init();
+            var memory = this.memory;
+            var pointer = this.pointer;
             var loop = "";
             var inLoop = false;
             var loopStack = [];
